c6d51e9a5c74a433ea9fb07aa6c01b59058c06f1
MinhPham204 <pnhatminh2004@gmail.com>
2025-09-27 19:44:22 +0700

    fix env

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,32 +1,32 @@
 ﻿import axios from "axios";
-
-const API_URL = "https://task-nest-rho.vercel.app/api"; // BE endpoint
+const baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+export const api = axios.create({ baseURL, withCredentials: true });
 
 // Lấy danh sách task với filter & phân trang
 export const getTasks = async (params = {}) => {
-  const res = await axios.get(API_URL, { params });
+  const res = await axios.get(baseURL, { params });
   return res.data;
 };
 
 // Thống kê (done/pending)
 export const getTaskStats = async () => {
-  const res = await axios.get(`${API_URL}/stats`);
+  const res = await axios.get(`${baseURL}/stats`);
   return res.data;
 };
 
 // Tạo mới task
 export const createTask = async (task) => {
-  const res = await axios.post(API_URL, task);
+  const res = await axios.post(baseURL, task);
   return res.data;
 };
 
 // Cập nhật task
 export const updateTask = async (id, task) => {
-  const res = await axios.put(`${API_URL}/${id}`, task);
+  const res = await axios.put(`${baseURL}/${id}`, task);
   return res.data;
 };
 
 // Xóa task
 export const deleteTask = async (id) => {
-  await axios.delete(`${API_URL}/${id}`);
+  await axios.delete(`${baseURL}/${id}`);
 };

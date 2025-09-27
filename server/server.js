a739a05787628daef4e744a519faeecefd870f8e
MinhPham204 <pnhatminh2004@gmail.com>
2025-09-27 18:02:03 +0700

    fix cors

--- server/server.js
+++ server/server.js
@@ -3,11 +3,30 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./src/config/db");
 
+
+
+const allowedOrigins = [
+  "https://tasknest-app.vercel.app", // domain chính
+  "https://task-nest-inz7-p9i5vckyu-pnhatminh2004-gmailcoms-projects.vercel.app", // domain preview
+  "http://localhost:5173" // để test local FE
+];
+
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Routes
